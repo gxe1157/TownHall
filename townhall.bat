28bd653b3815a1cb2d0508fa1503466f01fb1452
gxe1157 <gx1157@yahoo.com>
2017-06-09 23:31:46 -0400

    added bat file

--- townhall.bat
+++ townhall.bat
@@ -1,9 +1,9 @@
-cd C:\Users\Evelez\express\TownHall\dataSource
+cd C:\nodejsApps\TownHall\dataSource
 del .* /s /ah
 dir /s /ah
 cls
 
-cd C:\Users\Evelez\express\TownHall
-nodemon server.js
+cd C:\nodejsApps\TownHall
+node server.js
 
 cd\
